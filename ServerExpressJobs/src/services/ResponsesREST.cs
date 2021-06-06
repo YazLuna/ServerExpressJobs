c6a0375972bdb914ba9d1454f907a00c7891da04
Alemin <ale_200200@hotmail.com>
2021-06-05 20:05:19 -0500

    GET y DELETE de archivos
    Se implementaron los métodos para poder obtener uno o varios archivos y eliminarlos

---
+++ ServerExpressJobs/src/services/ResponsesREST.cs
@@ -0,0 +1,15 @@
+﻿namespace ServerExpressJobs.services
+{
+    enum ResponsesREST
+    {
+        CREATED = 201,
+        SUCCESSFUL = 200,
+        SERVER_ERROR = 500,
+        INVALID_INPUT = 400,
+        INVALID_REQUEST = 409,
+        NOT_AUTHORIZED = 403,
+        NOT_AUTHENTICATED = 401,
+        TIME_OUT = 419,
+        NOT_FOUND = 404,
+    }
+}
--- ServerExpressJobs/src/services/expressjobs.thrift
+++ ServerExpressJobs/src/services/expressjobs.thrift
@@ -9,11 +9,12 @@ struct Resource {
     4: string name
     5: int idService
     6: binary resourceFile
+    7: int idMemberATE
 }
 
 service ResourcesServices {
     int AddResource (1: Resource resource)
-    Resource GetResource (1: int idResource)
-    int DeleteResource (1: int idResource)
-    list<Resource> GetResourcesList(1: int idService)
+    Resource GetResource (1: string routeSave)
+    int DeleteResource (1: string routeSave)
+    list<Resource> GetResourcesList(1: list<string> routes)
 }
